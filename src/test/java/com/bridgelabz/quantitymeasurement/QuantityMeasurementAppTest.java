7230ea4f94fa3a8035f0b52e3567a3156f2dbbc3
skyy4 <skyonly4code@gmail.com>
2026-02-18 16:19:49 +0530

    Implement UC2: Feet and Inches Equality

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -6,35 +6,68 @@ import static org.junit.Assert.*;
 public class QuantityMeasurementAppTest {
 
     @Test
-    public void testEquality_SameValue() {
+    public void testFeetEquality_SameValue() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.0);
         assertTrue(feet1.equals(feet2));
     }
 
     @Test
-    public void testEquality_DifferentValue() {
+    public void testFeetEquality_DifferentValue() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(2.0);
         assertFalse(feet1.equals(feet2));
     }
 
     @Test
-    public void testEquality_NullComparison() {
+    public void testFeetEquality_NullComparison() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         assertFalse(feet1.equals(null));
     }
 
     @Test
-    public void testEquality_NonNumericInput() {
+    public void testFeetEquality_NonNumericInput() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         Object nonNumeric = new Object();
         assertFalse(feet1.equals(nonNumeric));
     }
 
     @Test
-    public void testEquality_SameReference() {
+    public void testFeetEquality_SameReference() {
         QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
         assertTrue(feet1.equals(feet1));
+    }
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(1.0);
+        assertTrue(inches1.equals(inches2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(2.0);
+        assertFalse(inches1.equals(inches2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(inches1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_NonNumericInput() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        Object nonNumeric = new Object();
+        assertFalse(inches1.equals(nonNumeric));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        assertTrue(inches1.equals(inches1));
     }
 }
